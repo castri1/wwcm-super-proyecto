10d6e8dd8e8d487c16e098ac6b5ededb63d426a2
Daniel Castrillon <daniel@melonn.com>
2022-10-22 09:51:23 -0500

    feat(random-words): Added files for logging random words

---
+++ generador-palabras.js
@@ -0,0 +1,47 @@
+const randomWords = require("random-words");
+
+
+async function logRandomWords() {
+  const randomWordsList = randomWords(50);
+
+  for (const word of randomWordsList) {
+    console.log(word);
+    await delay();
+  }
+}
+
+function delay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 50);
+  });
+}
+
+function logStart() {
+  console.log(`
+  ███████ ████████  █████  ██████  ████████ 
+  ██         ██    ██   ██ ██   ██    ██    
+  ███████    ██    ███████ ██████     ██    
+       ██    ██    ██   ██ ██   ██    ██    
+  ███████    ██    ██   ██ ██   ██    ██    
+                                            
+                                            
+  `);
+}
+
+function logEnd() {
+  console.log(`
+  ███████ ███    ██ ██████  
+  ██      ████   ██ ██   ██ 
+  █████   ██ ██  ██ ██   ██ 
+  ██      ██  ██ ██ ██   ██ 
+  ███████ ██   ████ ██████  
+                            
+                            
+    `);
+}
+
+(async () => {
+  logStart();
+  await logRandomWords();
+  logEnd();
+})();
